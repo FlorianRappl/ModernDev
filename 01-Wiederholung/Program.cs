00c3da94654d07dfe59c7046ebc876694a71281a
Florian Rappl <rappl.florian@gmail.com>
2012-12-29 17:03:36 +0100

    Setup
    Created the demonstration projects and implemented some paperwork stuff
    like copyright information and namespaces.

---
+++ 01-Wiederholung/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 
+ * (c) Florian Rappl, 2012.
+ * 
+ * This work is a demonstration for training purposes and may be used freely for private purposes.
+ * Usage for business training / workshops is prohibited without explicit permission from the author.
+ * 
+ */
+
+namespace ModernDev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+
+            for (var i = 0; i < 10; i++)
+                Console.WriteLine("{0} x {1} = {2}", a, i, a * i);
+        }
+    }
+}
---
+++ 02-Dispatching/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+/*
+ * 
+ * (c) Florian Rappl, 2012.
+ * 
+ * This work is a demonstration for training purposes and may be used freely for private purposes.
+ * Usage for business training / workshops is prohibited without explicit permission from the author.
+ * 
+ */
+
+namespace ModernDev
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
